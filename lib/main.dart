1328afcb82ea36840b65d0c54773c23b507ec062
DESKTOP-NR6TVAI\LAPTOP <amiraelsaftytraining@gmail.com>
2024-06-28 14:07:07 +0300

    Initial project. added empty home screen.

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_islami_fri_c11/home/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: HomeScreen.routeName,
+      routes: {HomeScreen.routeName: (context) => HomeScreen()},
+    );
+  }
+}
